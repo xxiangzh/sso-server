12e70ef20a74c24d2965e0cf43d300808be28c19
xzh <anenan@qq.com>
2020-11-13 17:04:28 +0800

    工具类

---
+++ src/main/java/com/xzh/sso/utils/SpringUtils.java
@@ -0,0 +1,26 @@
+package com.xzh.sso.utils;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author 向振华
+ * @date 2020/06/30 18:12
+ */
+@SuppressWarnings("unchecked")
+@Component
+public class SpringUtils implements ApplicationContextAware {
+
+    private static ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        SpringUtils.applicationContext = applicationContext;
+    }
+
+    public static <T> T getBean(Class<T> clazz) {
+        return applicationContext.getBean(clazz);
+    }
+}
